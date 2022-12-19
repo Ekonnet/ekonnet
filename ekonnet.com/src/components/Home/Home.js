ef28f5f3cdc06a6a06abf89fb5f49264d9dfa87d
Ekonnet <120484245+Ekonnet@users.noreply.github.com>
2022-12-19 07:31:08 +0000

    home

--- ekonnet.com/src/components/Home/Home.js
+++ ekonnet.com/src/components/Home/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 import Video from '../assets/Header_Video.mp4';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Swipper from './Swipper';
 import './Home.css';
+import TradeImg from "../assets/Trade.png";
 const Home = () => {
   return (
     <>
@@ -23,17 +26,44 @@ const Home = () => {
 
       <section className="slider">
         <Container>
-        <Swipper/>
-        <h2 className='digital-assets'>The home of digital asset trading</h2>
-       <div className="login-btn">
-        <Button size="lg" className='sign-up'>Sign Up</Button>
-        <Button size="lg" className='sign-in'>Sign In</Button>
-        </div>
+          <Swipper />
+          <h2 className='digital-assets'>The home of digital asset trading</h2>
+          <div className="login-btn">
+            <Button size="lg" className='sign-up'>Sign Up</Button>
+            <Button size="lg" className='sign-in'>Sign In</Button>
+          </div>
         </Container>
 
       </section>
 
- {/* slider section End here */}
+      {/* slider section End here */}
+
+
+      <section className='Coinlist'>
+
+        <h2> This section design later </h2>
+      </section>
+
+      {/* Trade section start here */}
+
+
+      <section className='trade'>
+        <Container>
+          <Row>
+            <Col className="col-lg-6 col-md-6 col-sm-12">
+              <div className='kuchbhi'>
+              <h2>Trade</h2>
+              <p>An intuitive interface for trading all your assets. Trade any of the currencies supported by Ekonnet or over 100 other IOUs on the XRP Ledger decentralized exchange. Explore and analyze the markets.The new Trade page works for you whether you're a beginner or a seasoned trader. </p>
+              </div>
+            </Col>
+            <Col className="col-lg-6 col-md-6 col-sm-12">
+            <img src={TradeImg} alt='' className='img-fluid'></img>
+            </Col>
+          </Row>
+        </Container>
+
+
+      </section>
 
 
 
