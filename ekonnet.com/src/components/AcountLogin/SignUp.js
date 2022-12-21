b84bcf9968576118571291ea3be1bf7d5ae1109e
Sunil Chouhan <116949657+Chouhan143@users.noreply.github.com>
2022-12-21 13:02:49 -0500

    miner

--- ekonnet.com/src/components/AcountLogin/SignUp.js
+++ ekonnet.com/src/components/AcountLogin/SignUp.js
@@ -18,7 +18,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="/">
-        Your Website
+       Ekonnet.com
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
