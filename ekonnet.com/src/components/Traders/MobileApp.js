628f3499cbe68150f17ef979e19f1c120b5152d1
sweeti-11 <120358979+sweeti-11@users.noreply.github.com>
2022-12-30 18:38:14 +0530

    mobile module complete

--- ekonnet.com/src/components/Traders/MobileApp.js
+++ ekonnet.com/src/components/Traders/MobileApp.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './MobileApp.css';
 import Button from 'react-bootstrap/Button';
 import Mobile from "../assets/Mobile-app.png";
-import PlayStore from '../assets/playstore-img.png';
-
-
+import mobile_crypto from "../assets/mobile_crypto.png";
+import mobile1 from "../assets/mobile1.png";
+import mobile2 from "../assets/mobile2.png";
+import mobile3 from "../assets/mobile3.png";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const MobileApp = () => {
+    useEffect(() => {
+        AOS.init();
+      }, [])
   return (
     <>
     <section id='mobile' className='mobile'>
@@ -20,37 +26,65 @@ const MobileApp = () => {
                   </p>
                   <Button className='get-start'>Trade Now</Button>
                 </div>
-                <div className='col-lg-6'>
+                <div className='col-lg-6' data-aos="zoom-in-up">
                     <img src={Mobile} alt='' className='img-fluid'></img>
                 </div>
             </div>
         </div>
     </section>
-    <section id='mob' className='mob'>
+    <section id='ultimate_crypto' className='ultimate_crypto'>
         <div className='container'>
             <div className='row'>
-                <div className='col-lg-12 text-center'>
-                    <h1>Trade with confidence on the world’s fastest
-                       and most secure crypto exchange
-                    </h1>
+                <div className='col-lg-6'>
+                    <h1>The ultimate crypto experience at your fingertips</h1>
+                    <h6>The Ekonnet mobile app provides the full functionality of the Ekonnet platform for seamless 'on-the-go' trading.</h6>
+                    <p>Access the world of cryptocurrency and explore its endless possibilities, including Fast Pay. Send and receive crypto payments with a simple and easy setup.</p>
                 </div>
-                <div className='d-flex'>
-                <div className='col-lg-6 mob_desk text-center'>
-                    <h1>Desktop</h1>
-                    <email>www.ekonnet.com</email>
-                 </div>
-                 <div className='col-lg-6 mob_desk'>
-                    <h1>Mobile App</h1>
-                    <img src={PlayStore} alt='playstore' className='img-fluid'></img>
-                 </div>
+                <div className='col-lg-6' data-aos="zoom-in-up">
+                <img src={mobile_crypto} alt='mobile_crypto' className='img-fluid'></img>
                 </div>
-
             </div>
-
         </div>
-
     </section>
-      
+    <section id='mobile1' className='mobile1'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-6' data-aos="zoom-in-up">
+                    <img src={mobile1} alt='mobile1' className='img-fluid'></img>
+                </div>
+                <div className='col-lg-6'>
+                    <h1>View positions, orders & history at a glance</h1>
+                    <p>The Ekonnet app gives you full control of your portfolio while you are on the move. View positions, open orders and trading history with a simple tap.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+      <section id='mobile2' className='mobile2'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-6'>
+                    <h1>Navigate the digital asset markets from anywhere</h1>
+                    <p>Trade an array of digital currencies from anywhere in the world. Ekonnet from simple and complex order types, margin trading and a number of additional features to preserve your market edge.</p>
+                </div>
+                <div className='col-lg-6' data-aos="zoom-in-up">
+                <img src={mobile2} alt='mobile2' className='img-fluid'></img>
+                </div>
+            </div>
+        </div>
+      </section>
+      <section id='mobile3' className='mobile3'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-lg-6' data-aos="zoom-in-up">
+                    <img src={mobile3} alt='mobile3' className='img-fluid'></img>
+                </div>
+                <div className='col-lg-6'>
+                    <h1>Ekonnet mobile app Lite Mode</h1>
+                    <p>Access the EKonnet trading platform. No clutter, no fuss! Easy to use and easy to trade, stake and lend crypto with just a few taps using a simplified interface.</p>
+                </div>
+            </div>
+        </div>
+    </section>
     </>
   )
 }
