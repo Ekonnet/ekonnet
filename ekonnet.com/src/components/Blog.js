2109ea4158b73fba01d763d24f4f349efd70865b
sweeti-11 <120358979+sweeti-11@users.noreply.github.com>
2023-01-06 18:32:50 +0530

    create module blog page

--- ekonnet.com/src/components/Blog.js
+++ ekonnet.com/src/components/Blog.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './Blog.css';
 import btc from './assets/btc.png';
 import {FaRegCalendarAlt, FaRegUser, FaRegComments, FaArrowRight} from 'react-icons/fa';
+import {SiBitcoinsv,SiEthereum} from 'react-icons/si';
+import {TbCurrencyDollar} from 'react-icons/tb';
+
 
 const Blog = () => {
   return (
@@ -18,8 +21,9 @@ const Blog = () => {
     <section id='blog_img' className='blog_img'>
       <div className='container'>
         <div className='row'>
-          <div className='col-lg-8 d-flex'>
-            <div className=''>
+          <div className='col-lg-9'>
+            <div className='d-flex'>
+            <div>
               <img src={btc} alt='blog-img' className='img-fluid'></img>
             </div>   
             <div>
@@ -35,17 +39,14 @@ const Blog = () => {
               <p>Nemo enim ipsam voluptatem quia volup tas sit aspernatur aut odit aut fu sed quia conse quun tur magni dolor quia. In scelerisque turpis vel bibendum […]</p>
               <button className='btn btn-primary d-flex'>Continue Reading<FaArrowRight/>
               </button>
-            </div>   
-          </div>
-          <div className='col-lg-4'>
-           <h1>dgtdtfj</h1>
-          </div>
-          <div className='col-lg-8 d-flex'>
-            <div className=''>
-            <img src={btc} alt='blog-img' className='img-fluid'></img>
+            </div> 
             </div>
+            <div className='d-flex'>
             <div>
-              <h1>Bitcoin vs Ethereum</h1>
+              <img src={btc} alt='blog-img' className='img-fluid'></img>
+            </div>   
+            <div>
+              <h1>What is Bitcoin Mining</h1>
               <div className='d-flex blog_icons'>
               <FaRegCalendarAlt/>
               <span> January 3, 2023</span>
@@ -53,14 +54,70 @@ const Blog = () => {
               <span>Robert Downey</span>
               <FaRegComments/>
               <span>1</span>
-              </div>
+              </div>  
               <p>Nemo enim ipsam voluptatem quia volup tas sit aspernatur aut odit aut fu sed quia conse quun tur magni dolor quia. In scelerisque turpis vel bibendum […]</p>
               <button className='btn btn-primary d-flex'>Continue Reading<FaArrowRight/>
               </button>
-            </div>   
+            </div> 
+           </div>
+           </div>
+          <div className='col-lg-3 search_btn'>
+          <input type='text' placeholder='Search...'></input>
+          <button className='btn btn-primary'>Search</button>
+          <h3>Crypto Tickers</h3>
+          <div className='d-flex'>
+            <h5>Crypto Currency</h5>
+            <h5>Price</h5>
           </div>
-        </div>
-      </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiBitcoinsv/>
+          <h6>Bitcoin <span>(BTC)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>6855.83</h4>
+          </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiEthereum/>
+          <h6>Ethereum <span>(ETH)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>416.979</h4>
+          </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiBitcoinsv/>
+          <h6>Algorand <span>(ALGO)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>0.48167</h4>
+          </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiBitcoinsv/>
+          <h6>Toncoin <span>(TON)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>652.101</h4>
+          </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiBitcoinsv/>
+          <h6>Litecoin <span>(LTC)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>114.335</h4>
+          </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiBitcoinsv/>
+          <h6>Cardano <span>(ADA)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>0.15502</h4>
+          </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiBitcoinsv/>
+          <h6>Monero <span>(XMR)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>166.379</h4>
+          </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiBitcoinsv/>
+          <h6>Polygon <span>  (MATIC)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>301.285</h4>
+          </div>
+          <div className='d-flex sidebar_iconss'>
+          <SiBitcoinsv/>
+          <h6>ApeCoin <span>(APE)</span></h6>
+          <h4 className='d-flex sidebar_ico'><TbCurrencyDollar/>0.23586</h4>
+          </div>
+          </div>
+          </div>
+          
+          </div> 
     </section>
     </>
   )
