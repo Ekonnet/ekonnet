0d1d15958c67adb44af16684116e09d1530b61ec
sweeti-11 <120358979+sweeti-11@users.noreply.github.com>
2023-01-12 12:48:40 +0530

    media query on features module

--- ekonnet.com/src/components/Traders/Features.js
+++ ekonnet.com/src/components/Traders/Features.js
@@ -28,13 +28,14 @@ const Features = () => {
             </div>
         </div>
       </section>
-      <section id='exchangee' className='exchangee bg-[#131128] pt-[100px] ' >
+      <section id='exchangee' className='exchangee bg-[#131128] pt-[70px] ' >
         <div className='container'>
             <div className='row'>
                 <div className='col-lg-6 ex'>
                     <h1>Exchange</h1>
                     <p>Ekonnet offers order books with top tier liquidity, allowing users to easily exchange Bitcoin, Ethereum, EOS, Litecoin, Ripple, NEO and many other digital assets 
-                       with minimal slippage. Ekonnet also boasts a suite of order types to help traders take advantage of every situation
+                       with minimal slippage. <br/>Ekonnet also boasts a suite of order types to help traders take advantage of every situation.<br/>
+                       Ekonnet also boasts a suite of order types to help traders take advantage of every situation.
                     </p>
                 </div>
                 <div className='col-lg-6'>
@@ -43,13 +44,13 @@ const Features = () => {
             </div>
         </div>
       </section>
-      <section id='margin_trading' className='margin_trading bg-[#131128] pt-[100px]'>
+      <section id='margin_trading' className='margin_trading bg-[#131128] pt-[70px]'>
         <div className='container'>
             <div className='row'>
-                <div className='col-lg-6'>
+                <div className='col-lg-6 order-lg-1 order-2'>
                 <img src={Margin} alt='' className='img-fluid mx-auto'></img>
                 </div>
-                <div className='col-lg-6 ex'>
+                <div className='col-lg-6 ex odrer-lg-2 order-1'>
                     <h1>Margin trading</h1>
                     <p>Ekonnet allows users to trade with up to 10x leverage by receiving funding from the peer to peer margin funding platform.<br />
                        Users can enter an order to borrow the desired amount of funding at the rate and duration of their choice, or they can simply open a position and Ekonnet will take out funding for them at the best available rate at that time.
@@ -58,7 +59,7 @@ const Features = () => {
             </div>
         </div>
       </section>
-      <section id='margin_funding' className='margin_funding bg-[#131128] pt-[100px]'>
+      <section id='margin_funding' className='margin_funding bg-[#131128] pt-[70px]'>
         <div className='container'>
             <div className='row'>
                 <div className='col-lg-6 ex'>
@@ -73,13 +74,13 @@ const Features = () => {
             </div>
         </div>
       </section>
-      <section id='Derivatives' className='Derivatives bg-[#131128] pt-[100px]'>
+      <section id='Derivatives' className='Derivatives bg-[#131128] pt-[70px]'>
         <div className='container'>
             <div className='row'>
-                <div className='col-lg-6'>
+                <div className='col-lg-6 order-lg-1 order-2'>
                 <img src={DerivativesImg} alt='' className='img-fluid mx-auto'></img>
                 </div>
-                <div className='col-lg-6 ex'>
+                <div className='col-lg-6  order-lg-2 order-1 ex'>
                     <h1>Derivatives</h1>
                     <p>Users wishing to mitigate the risks associated with the volatility of Digital Tokens may trade Derivative Products on the Derivatives Site. Perpetual contracts are already available and further Derivative Products are under development.<br />
                        Derivative Products can be traded using a Derivative Wallet credited with USDt0, a Digital Token which can be obtained by converting USDt, as explained in more details on our Knowledge Base.
