717abe41a910b20f175e59efd689fdb4303fac55
sweeti-11 <120358979+sweeti-11@users.noreply.github.com>
2023-01-30 11:59:59 +0530

    complete module on everywhere

--- ekonnet.com/src/components/Best.js
+++ ekonnet.com/src/components/Best.js
@@ -91,7 +91,7 @@ const Best = () => {
         </div>
         </div>
     </section>
-    {/* <section id='best5' className='best5  bg-[#131128] pt-[1px]'>
+    <section id='best5' className='best5  bg-[#131128] pt-[1px]'>
         <div className='container'>
         <div className='row'>
         <span className='text-center'>Upcoming perks in the pipeline</span>
@@ -130,6 +130,7 @@ const Best = () => {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
            <div className='col-lg-4'> 
+        </div>
         </div>
         </div>
     </section>
